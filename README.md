a5d952681d1aedabe2e04433602de1dea46b578f
Bakr Mohamed <engbakrmohamed@hotmail.com>
2024-09-26 17:31:48 +0300

    Add descriptive descriptions and ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# BMKeychain
-Handle Keychain managments
+# KeychainHelper
+
+`KeychainHelper` is a Swift utility that simplifies saving, retrieving, updating, and deleting sensitive data in the iOS Keychain. This helper struct uses Apple's Security framework and is designed to handle keychain operations in a clean and secure way.
+
+## Features
+
+- **Save value**: Store sensitive data securely in the Keychain.
+- **Retrieve value**: Fetch stored data from the Keychain.
+- **Update value**: Modify existing data stored in the Keychain.
+- **Delete value**: Remove stored data from the Keychain.
+
+## Installation
+
+### Swift Package Manager
+
+You can install `BMKeychain` by adding it via the Swift Package Manager (SPM) in Xcode.
+
+#### Adding to a Project
+
+1. In Xcode, select your project in the navigator.
+2. Choose **Package Dependencies**.
+3. Click the **+** button to add a new package.
+4. Enter the following URL:  
+   `https://github.com/BakrIOS91/BMKeychain`
+5. Select the relevant version and click **Add Package**.
+6. Once added, you can import `BMKeychain` where needed in your project:
+
+   ```swift
+   import BMKeychain
+   ```
+
+#### Adding to a Swift Package
+
+To include `BMKeychain` as a dependency in your own Swift package, add the following to your `Package.swift` file:
+
+```swift
+dependencies: [
+    .package(url: "https://github.com/BakrIOS91/BMKeychain", from: "0.1.1")
+]
+```
+
+Then include `BMKeychain` as a dependency in your targets:
+
+```swift
+.target(
+    name: "YourTarget",
+    dependencies: ["BMKeychain"]
+)
+```
+
+## Usage
+
+### 1. Saving a Value
+Use `saveValue(_:forKey:)` to store a string in the Keychain. If a value already exists for the given key, it will be updated.
+
+```swift
+try KeychainHelper.shared.saveValue("password123", forKey: "userPassword")
+```
+
+### 2. Retrieving a Value
+Retrieve a stored value from the Keychain using `retrieveValue(forKey:)`. It returns an optional `String`.
+
+```swift
+let password = try KeychainHelper.shared.retrieveValue(forKey: "userPassword")
+```
+
+### 3. Updating a Value
+Update an existing value using `updateValue(_:forKey:)`.
+
+```swift
+try KeychainHelper.shared.updateValue("newPassword456", forKey: "userPassword")
+```
+
+### 4. Deleting a Value
+Delete a stored value using `deleteValue(forKey:)`.
+
+```swift
+try KeychainHelper.shared.deleteValue(forKey: "userPassword")
+```
+
+## Error Handling
+
+`KeychainHelper` uses custom errors defined as `KeychainError` to handle various failure cases like:
+
+- case failedToConvertValueToData
+- case failedToSaveValue
+- case failedToRetrieveValue
+- case faildToDeleteValue
+- case failedToUpdateValue
+
+Make sure to handle these errors appropriately in your application.
--- Sources/BMKeychain/KeyChainManager.swift
+++ Sources/BMKeychain/KeyChainManager.swift
@@ -8,72 +8,106 @@
 import Foundation
 import Security
 
+/// A helper struct for interacting with the iOS Keychain to securely store, update, retrieve, and delete sensitive data.
 @MainActor
 public struct KeychainHelper {
+    
+    /// Singleton instance of KeychainHelper to provide easy access to its methods.
     public static let shared = KeychainHelper()
     
+    /// The service name for the Keychain entries, using the app's bundle identifier. Defaults to "com.keychain.service" if unavailable.
     private let service: String = Bundle.main.bundleIdentifier ?? "com.keychain.service"
     
+    /// Saves a value in the Keychain for a specified key.
+    /// - Parameters:
+    ///   - value: The string value to save in the Keychain.
+    ///   - forKey: The unique key associated with the value.
+    /// - Throws: Throws `KeychainError.failedToConvertValueToData` if the string cannot be converted to `Data`.
+    ///           Throws `KeychainError.failedToSaveValue` if the save operation fails.
+    ///
+    /// If a value already exists for the given key, it will update the value instead.
     func saveValue(_ value: String, forKey: String) throws {
-        if try retrieveValue(forKey: forKey)?.isEmpty  ?? true {
+        // Check if the value already exists for the given key
+        if try retrieveValue(forKey: forKey)?.isEmpty ?? true {
+            // Convert the string value to Data for secure storage
             guard let data = value.data(using: .utf8) else {
                 throw KeychainError.failedToConvertValueToData
             }
             
+            // Keychain query dictionary to define the save operation
             let query: [String: Any] = [
-                kSecClass as String: kSecClassGenericPassword,
-                kSecAttrService as String: service,
-                kSecAttrAccount as String: forKey,
-                kSecValueData as String: data
+                kSecClass as String: kSecClassGenericPassword,   // Specify the keychain item class
+                kSecAttrService as String: service,             // Define the service (bundle identifier)
+                kSecAttrAccount as String: forKey,              // Key used to identify the item
+                kSecValueData as String: data                   // Data to store (converted from string)
             ]
             
+            // Attempt to add the item to the Keychain
             let status = SecItemAdd(query as CFDictionary, nil)
             if status != errSecSuccess {
                 throw KeychainError.failedToSaveValue
             }
         } else {
+            // If value exists, update it instead
             try updateValue(value, forKey: forKey)
         }
-        
     }
     
+    /// Updates an existing value in the Keychain for a specified key.
+    /// - Parameters:
+    ///   - value: The new string value to update in the Keychain.
+    ///   - forKey: The unique key associated with the value.
+    /// - Throws: Throws `KeychainError.failedToConvertValueToData` if the string cannot be converted to `Data`.
+    ///           Throws `KeychainError.failedToUpdateValue` if the update operation fails.
     func updateValue(_ value: String, forKey: String) throws {
+        // Convert the string value to Data
         guard let data = value.data(using: .utf8) else {
             throw KeychainError.failedToConvertValueToData
         }
         
+        // Keychain query to find the item to update
         let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: service,
-            kSecAttrAccount as String: forKey
+            kSecClass as String: kSecClassGenericPassword,   // Specify the keychain item class
+            kSecAttrService as String: service,             // Service identifier
+            kSecAttrAccount as String: forKey               // Key used to find the item
         ]
         
+        // Fields to update (new data)
         let updateFields: [String: Any] = [
-            kSecValueData as String: data
+            kSecValueData as String: data                   // New data to update in the keychain
         ]
         
+        // Attempt to update the item in the Keychain
         let status = SecItemUpdate(query as CFDictionary, updateFields as CFDictionary)
         if status != errSecSuccess {
             throw KeychainError.failedToUpdateValue
         }
     }
     
+    /// Retrieves a value from the Keychain for a specified key.
+    /// - Parameter forKey: The unique key associated with the value to retrieve.
+    /// - Returns: The stored string value, or `nil` if no value exists.
+    /// - Throws: Throws `KeychainError.failedToRetrieveValue` if the retrieval operation fails.
     func retrieveValue(forKey: String) throws -> String? {
+        // Keychain query to search for the item
         let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: service,
-            kSecAttrAccount as String: forKey,
-            kSecMatchLimit as String: kSecMatchLimitOne,
-            kSecReturnData as String: true
+            kSecClass as String: kSecClassGenericPassword,   // Specify the keychain item class
+            kSecAttrService as String: service,             // Service identifier
+            kSecAttrAccount as String: forKey,              // Key used to find the item
+            kSecMatchLimit as String: kSecMatchLimitOne,    // Limit the search to one matching item
+            kSecReturnData as String: true                  // Return the stored data if found
         ]
         
         var result: AnyObject?
+        // Attempt to retrieve the matching item from the Keychain
         let status = SecItemCopyMatching(query as CFDictionary, &result)
         
+        // Ensure the operation succeeded and the result is not nil
         guard status == errSecSuccess, let data = result as? Data else {
             throw KeychainError.failedToRetrieveValue
         }
         
+        // Convert the retrieved data back into a string
         if let value = String(data: data, encoding: .utf8) {
             return value
         } else {
@@ -81,13 +115,18 @@ public struct KeychainHelper {
         }
     }
     
+    /// Deletes a value from the Keychain for a specified key.
+    /// - Parameter forKey: The unique key associated with the value to delete.
+    /// - Throws: Throws `KeychainError.faildToDeleteValue` if the delete operation fails.
     func deleteValue(forKey: String) throws {
+        // Keychain query to identify the item to delete
         let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: service,
-            kSecAttrAccount as String: forKey
+            kSecClass as String: kSecClassGenericPassword,   // Specify the keychain item class
+            kSecAttrService as String: service,             // Service identifier
+            kSecAttrAccount as String: forKey               // Key used to find the item
         ]
         
+        // Attempt to delete the item from the Keychain
         let status = SecItemDelete(query as CFDictionary)
         if status != errSecSuccess {
             throw KeychainError.faildToDeleteValue
